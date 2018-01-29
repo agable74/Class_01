f1225f6c1c37e9f1d821585611aeae2cee6d76b9
Alex Gable <agable@ithaca.edu>
2018-01-29 12:42:00 -0500

    Added single value tests

--- test/edu/ithaca/agable/stringCalculatorTest.java
+++ test/edu/ithaca/agable/stringCalculatorTest.java
@@ -12,6 +12,13 @@ class stringCalculatorTest {
         String emptyString = "";
         assertEquals(0,myStringCalculator.Add(emptyString),"Does not properly work with empty string");
 
+        String oneStringPos = "1";
+        String oneStringNeg = "-1";
+        String oneStringZero = "0";
+
+        assertEquals(1,myStringCalculator.Add(oneStringPos),"Does not add single positive value");
+        assertEquals(-1,myStringCalculator.Add(oneStringNeg),"Does not add single negative value");
+        assertEquals(0,myStringCalculator.Add(oneStringZero),"Does not add single zero value");
 
     }
 
