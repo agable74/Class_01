644cacb3552880ef6f2670a9bb6ee18f246c3af8
Alex Gable <agable@ithaca.edu>
2018-01-29 12:35:38 -0500

    Implemented empty string functionality

--- src/edu/ithaca/agable/stringCalculatorImpl.java
+++ src/edu/ithaca/agable/stringCalculatorImpl.java
@@ -3,6 +3,9 @@ package edu.ithaca.agable;
 public class stringCalculatorImpl implements stringCalculator{
 
     public int Add(String numbers){
+        if(numbers == ""){
+            return 0;
+        }
         return -1;
     }
 }
