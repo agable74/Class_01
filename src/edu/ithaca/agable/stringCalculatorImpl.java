d68f82a62e89c61bd9232d7e95c3e4bc8349cb09
Alex Gable <agable@ithaca.edu>
2018-01-30 10:33:33 -0500

    Implemented for 2 numbers

--- src/edu/ithaca/agable/stringCalculatorImpl.java
+++ src/edu/ithaca/agable/stringCalculatorImpl.java
@@ -34,6 +34,79 @@ public class stringCalculatorImpl implements stringCalculator{
                 }
             }
         }
+        else {
+            int totalNum = 0;
+
+            int numNumbers = 1;
+            for (int j = 0; j < numbers.length(); j++) {
+                if (numbers.charAt(j) == ',') {
+                    numNumbers++;
+                }
+            }
+
+            int startIndex = 0;
+            for (int j = 0; j < numNumbers-1; j++) {
+                for (int i = 0; i < numbers.length(); i++) {
+                    if (numbers.charAt(i) == ',') {
+                        startIndex = i;
+                    }
+                }
+
+                String potentialNum = numbers.substring(startIndex+1);
+                numbers = numbers.substring(0, startIndex);
+
+                if(!potentialNum.contains("-")){
+                    //positive
+                    boolean isDigit = true;
+                    for(int i = 0; i < potentialNum.length(); i++){
+                        if(!isDigit(potentialNum.charAt(i))){
+                            isDigit = false;
+                        }
+                    }
+                    if(isDigit){
+                        totalNum+= Integer.parseInt(potentialNum);
+                    }
+                }else{
+                    //negative
+                    boolean isDigit = true;
+                    for(int i = 1; i < potentialNum.length(); i++){
+                        if(!isDigit(potentialNum.charAt(i))){
+                            isDigit = false;
+                        }
+                    }
+                    if(isDigit && potentialNum.charAt(0)=='-'){
+                        totalNum += Integer.parseInt(potentialNum);
+                    }
+                }
+            }
+            if(!numbers.contains(",")){
+                //single values
+                if(!numbers.contains("-")){
+                    //positive
+                    boolean isDigit = true;
+                    for(int i = 0; i < numbers.length(); i++){
+                        if(!isDigit(numbers.charAt(i))){
+                            isDigit = false;
+                        }
+                    }
+                    if(isDigit){
+                        totalNum+= Integer.parseInt(numbers);
+                    }
+                }else{
+                    //negative
+                    boolean isDigit = true;
+                    for(int i = 1; i < numbers.length(); i++){
+                        if(!isDigit(numbers.charAt(i))){
+                            isDigit = false;
+                        }
+                    }
+                    if(isDigit && numbers.charAt(0)=='-'){
+                        totalNum += Integer.parseInt(numbers);
+                    }
+                }
+            }
+            return totalNum;
+        }
         return -1;
     }
 }
